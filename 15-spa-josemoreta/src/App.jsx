f0ff111e8cbf097e00b4d4e36fe3663a318e9ff3
jmoreta <josemoreta27@gmail.com>
2025-04-21 16:23:08 -0400

    Add a testing project

---
+++ 15-spa-josemoreta/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1 className='appTitle'>TODO  APP</h1>  
+    <main>
+     
+      <section className='taskBoard'> 
+        <section className='taskStatus'>
+        1
+        </section>
+        <section className='taskStatus'>
+        2
+        </section>
+        <section className='taskStatus'>
+        3
+        </section>
+      </section>
+
+    </main>
+    </>
+  )
+}
+
+export default App
